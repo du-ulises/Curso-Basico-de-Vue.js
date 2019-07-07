cf4343cffbe7c46173ae292f3190b2b9ed1dfac9
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-06 23:16:51 -0500

    Configurando la API de Lastfm en Vue.js

--- platzimusic/src/App.vue
+++ platzimusic/src/App.vue
@@ -1,57 +1,55 @@
 <template lang="pug">
   #app
     img(src='./assets/logo.png')
-    h1 {{ msg }}
-    h2 Essential Links
+    h1 PlatziMusic
     ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://chat.vuejs.org', target='_blank') Community Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
-
+      li(v-for="artist in artists") {{ artist.name }}
 </template>
 
 <script>
+import getArtist from "./api";
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      artists: []
+    };
+  },
+  mounted: function() {
+    const self = this;
+    getArtist().then(function(artists) {
+      self.artists = artists;
+    });
   }
-}
+};
 </script>
 
 <style lang="stylus">
-#app 
-	font-family 'Avenir', Helvetica, Arial, sans-serif
-	-webkit-font-smoothing antialiased
-	-moz-osx-font-smoothing grayscale
-	text-align center
-	color #2c3e50
-	margin-top 60px
-h1, h2 
-	font-weight normal
-ul 
-	list-style-type none
-	padding 0
-li 
-	display inline-block
-	margin 0 10px
-a 
-	color #42b983
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
---
+++ platzimusic/src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '86d72feb921b0f1543f1fb7256a7b772',
+    secret: '2ebb18f2d597e56f47e4a73c386b25a0',
+    registeredTo: 'Diego-Ulises'
+}	

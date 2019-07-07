4253bbee9272223be8c0ff3cba72e271e873453d
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-07 01:25:39 -0500

    Fixes por la URL

--- platzimusic/src/App.vue
+++ platzimusic/src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://diego-ulises.github.io/Curso-Basico-de-Vue.js/platzimusic/dist/logo.png')
     h1 PlatziMusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" :value="country.value") {{ country.name }}

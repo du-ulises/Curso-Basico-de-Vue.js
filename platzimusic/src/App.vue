dde39f6caa786a629ecbc1c352a2153fbeb66dfe
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-07 00:51:57 -0500

    Actualizando datos en la interfaz con Vue.js

--- platzimusic/src/App.vue
+++ platzimusic/src/App.vue
@@ -2,25 +2,47 @@
   #app
     img(src='./assets/logo.png')
     h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{ country.name }}
     ul
-      li(v-for="artist in artists") {{ artist.name }}
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
 
 <script>
-import getArtist from "./api";
+import Artist from "./components/Artist.vue";
+import getArtists from "./api";
 
 export default {
   name: "app",
   data() {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        { name: "Argentina", value: "argentina" },
+        { name: "Mexico", value: "mexico" },
+        { name: "España", value: "spain" }
+      ],
+      selectedCountry: "mexico"
     };
   },
-  mounted: function() {
-    const self = this;
-    getArtist().then(function(artists) {
-      self.artists = artists;
-    });
+  components: {
+    Artist
+  },
+  methods: {
+    refreshArtists() {
+      const self = this;
+      getArtists(this.selectedCountry).then(function(artists) {
+        self.artists = artists;
+      });
+    }
+  },
+  mounted() {
+    this.refreshArtists();
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists();
+    }
   }
 };
 </script>
